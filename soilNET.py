72c94bd4d3b02aa4edc1771e1cd01a4e643864a5
Rashi Anubhi Srivastava <rashisrivastava2001@gmail.com>
2021-01-31 22:43:49 +0530

    Add files via upload

--- soilNET.py
+++ soilNET.py
@@ -20,8 +20,8 @@ img_width, img_height = 150, 150
 
 #train test splitting
 
-train_data = "Soils/Train"
-test_data = "Soils/Test"
+train_data = "/home/rashi/Documents/AI_based_Crop_recommendation_System/Soils/Train"
+test_data = "/home/rashi/Documents/AI_based_Crop_recommendation_System/Soils/Test"
 
 train_path = list(paths.list_images(train_data)) 
 totalTrain = len(train_path)
